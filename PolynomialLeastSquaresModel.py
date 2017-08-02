adf9fb966a40b65beede33ad18040506525735d0
alexstennet <alexstennet@berkeley.edu>
2017-08-02 13:01:14 -0700

    polynomial least squares model based on the LLS model provided

---
+++ PolynomialLeastSquaresModel.py
@@ -0,0 +1,21 @@
+from ransac import LinearLeastSquaresModel
+from sklearn.preprocessing import PolynomialFeatures
+
+class PolynomialLeastSquaresModel(LinearLeastSquaresModel):
+    """docstring for PolynomialLeastSquaresModel."""
+    def __init__(self, input_columns, output_columns, deg):
+        super(self, input_columns, output_columns)
+        self.poly = PolynomialFeatures(degree=2)
+    def fit(self, data):
+        A = numpy.vstack([data[:,i] for i in self.input_columns]).T
+        B = numpy.vstack([data[:,i] for i in self.output_columns]).T
+        A = self.poly.fit_transform(A)
+        x,resids,rank,s = scipy.linalg.lstsq(A,B)
+        return x
+    def get_error(self, data, model):
+        A = numpy.vstack([data[:,i] for i in self.input_columns]).T
+        B = numpy.vstack([data[:,i] for i in self.output_columns]).T
+        A = self.poly.fit_transform(A)
+        B_fit = scipy.dot(A, model)
+        err_per_point = numpy.sum((B-B_fit)**2,axis=1) # sum squared error per row
+        return err_per_point
