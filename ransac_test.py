4f8cb3242c42a913f603e950881ca17aa905a02b
alexstennet <alexstennet@berkeley.edu>
2017-08-07 11:09:40 -0700

    made look nicer

--- ransac_test.py
+++ ransac_test.py
@@ -12,9 +12,17 @@ drive = "0001"
 
 velos = pykitti.raw(basedir, date, drive).velo
 
-velo = next(velos)
-velo = next(velos)
-print(len(velo))
+
+def get_velo(i, velos):
+    velo = next(velos)
+    while i > 0:
+        velo = next(velos)
+        i -= 1
+    return velo
+
+
+i = 0
+velo = get_velo(i, velos)
 
 # velo = velo[velo[:,3] > .05]
 
@@ -22,14 +30,26 @@ print(len(velo))
 
 velo = np.delete(velo, 3, axis=1)
 
-x = np.delete(velo, 2, axis=1)
-y = velo[:,2]
+def fit(data, iterations, threshold):
+    model = PolynomialLeastSquaresModel([0,1], [2], 2)
+    ransac_fit, ransac_data = ransac(data,model,
+                                     n=20,
+                                     k=iterations,
+                                     t=threshold, # threshold
+                                     d=7000,
+                                     m=200, #
+                                     return_all=True)
+    return velo[ransac_data['inliers']]
 
-# model = ransac.LinearLeastSquaresModel([0,1], [2])
-model = PolynomialLeastSquaresModel([0,1], [2], 2)
+seg = fit(velo, 100, .25)
+np.savetxt('./{}1.txt'.format(i), seg)
 
-ransac_fit, ransac_data = ransac(velo,model,
-                                 n=20, k=500, t=.1, d=7000, m=200, # misc. parameters
-                                 return_all=True)
-data = np.concatenate((x[ransac_data['inliers']], np.array([y[ransac_data['inliers']]]).T), axis=1)
-np.savetxt('./' + 'blah' + drive + '.txt', velo[ransac_data['inliers']])
+seg2 = fit(seg, 200, .1)
+np.savetxt('./{}2.txt'.format(i), seg2)
+
+seg3 = fit(seg2, 400, .075)
+np.savetxt('./{}3.txt'.format(i), seg3)
+
+seg4 = fit(seg3, 800, .05)
+np.savetxt('./{}4.txt'.format(i), seg4)
+#
